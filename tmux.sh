27aee26b940d45c99ca0a05a1ec2d08e0d5a400f
Matthew Zhou <hhs66317@gmail.com>
2019-01-05 16:27:17 +0800

    新增 tmux.sh

---
+++ tmux.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# 兼容zsh
+# export DISABLE_AUTO_TITLE="true"
+
+session="work"
+
+tmux has-session -t $session
+if [ $? = 0 ];then
+    tmux attach-session -t $session
+    exit
+fi
+
+tmux new -d -s $session
+
+tmux renamew -t $session:1 ros
+tmux select-window -t $session:ros
+tmux source-file ~/.tmux/mylayout
+tmux send-keys -t $session:ros.1 'fish' Enter
+tmux send-keys -t $session:ros.2 'fish' Enter
+tmux send-keys -t $session:ros.3 'fish' Enter
+
+tmux new-window -t $session:2 -n chat
+tmux select-window -t $session:chat
+tmux source-file ~/.tmux/mylayout
+tmux send-keys -t $session:chat.1 'fish' C-m
+tmux send-keys -t $session:chat.1 'cd /home/hack.chat;node server.js' C-m
+tmux send-keys -t $session:chat.2 'fish' C-m
+tmux send-keys -t $session:chat.2 'cd /home/hack.chat/client;http-server -p 11111' C-m
+tmux send-keys -t $session:chat.3 'fish' Enter
+
+tmux new-window -t $session:3 -n mojo
+tmux select-window -t $session:mojo
+tmux source-file ~/.tmux/mylayout
+tmux send-keys -t $session:mojo.1  'fish' C-m
+tmux send-keys -t $session:mojo.1  'cd /home/mojo_web;morbo Mojo_web.pl' C-m
+tmux send-keys -t $session:mojo.2  'fish' C-m
+tmux send-keys -t $session:mojo.3  'fish' C-m
+
+tmux new-window -t $session:4 -n excel
+tmux select-window -t $session:excel
+tmux source-file ~/.tmux/mylayout
+tmux send-keys -t $session:excel.1  'fish' C-m
+tmux send-keys -t $session:excel.1  'cd /usr/local/node-v8.5.0-linux-x64/lib/node_modules/ethercalc/bin;ethercalc' C-m
+tmux send-keys -t $session:excel.2  'fish' C-m
+tmux send-keys -t $session:excel.3  'fish' C-m
+
+tmux new-window -t $session:5 -n upload
+tmux select-window -t $session:upload
+tmux source-file ~/.tmux/mylayout
+tmux send-keys -t $session:upload.1  'fish' C-m
+tmux send-keys -t $session:upload.1  'cd /home/upload;morbo -m production -l http://[::]:4000 ./upload.pl' C-m
+tmux send-keys -t $session:upload.2  'fish' C-m
+tmux send-keys -t $session:upload.2  'cd /home/upload;morbo -m production -l http://[::]:4001 ./android_upload.pl' C-m
+tmux send-keys -t $session:upload.3  'fish' C-m
+
+tmux select-window -t $session:ros
+tmux attach-session -t $session
+
